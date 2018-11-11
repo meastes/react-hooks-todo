47cf3a8137c49eb1eb6d8bf23b7c1159cb63e80d
Mike Eastes <michael.eastes@gmail.com>
2018-11-11 09:10:37 -0600

    Extracts "All" Toggle.

--- src/components/Todo/components/List/List.js
+++ src/components/Todo/components/List/List.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Item from './components/Item';
+import ToggleAll from './components/ToggleAll';
 
 export default ({ todos }) => (
   <div>
-    <input id="toggle-all" className="toggle-all" type="checkbox" />
-    <label for="toggle-all">Mark all as complete</label>
+    <ToggleAll />
     <ul className="todo-list">
       {todos.map(({ description, completed }) => (
         <Item description={description} completed={completed} />
---
+++ src/components/Todo/components/List/components/ToggleAll/ToggleAll.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <input id="toggle-all" className="toggle-all" type="checkbox" />
+    <label for="toggle-all">Mark all as complete</label>
+  </>
+);
